05ac169d3bcf3222ca0fa991dbd5677611a61c13
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2026-02-12 00:05:55 +0300

    feat: tailwind test

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-  <h1 class='text-6xl font-bold'>Hello tailwind</h1>
+<button class="rounded bg-orange-500 px-2 border-white/50 border-solid">click me</button>
+<div class="flex items-center gap-5 blur">
+  <div class="bg-blue-300 w-20 h-20"></div>
+  <div class="bg-blue-300 w-20 h-20"></div>
+</div>
 </template>
 
 <style scoped>
