03ef00b3a402d4dbe3e7751a70af8ab3a15a636a
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2026-02-12 23:04:06 +0300

    feat: add button

---
+++ src/1_app/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Button from '../6_shared/ui/Button.vue'
+// настроить алиасы
+// настроить еслинт претир + prettier-plugin-tailwindcss
+// настроить стейт менеджер 
+// настроить роутинг
+// написать лайаут 
+</script>
+
+<template>
+  <Button>Кнопка</Button>
+</template>
+
+<style scoped></style>
---
+++ src/6_shared/ui/Button.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class=" px-5 py-2.5 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm transition-colors duration-200 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2 dark:bg-gray-100 dark:text-gray-900 dark:hover:bg-gray-200"><slot></slot></button>
+</template>
+
+<style scoped></style>
